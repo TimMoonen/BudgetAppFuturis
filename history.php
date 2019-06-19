e95e4ef3c895b31c6bc806f86c7dc940a7e4f884
TimMoonen <50866040+TimMoonen@users.noreply.github.com>
2019-06-19 11:36:55 +0200

    Create history.php

---
+++ history.php
@@ -0,0 +1,198 @@
+<?php include ("dbh.php") ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:100,300,400,600" rel="stylesheet" type="text/css">
+        <link href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet" type="text/css">
+        <link type="text/css" rel="stylesheet" href="style.css">
+        <title>Budget App Futuris</title>
+    </head>
+    <body>
+
+          <?php
+        $month = "Juni";
+    ?>
+        
+       <div class="top">
+            <div class="budget">
+                <div class="budget__title">
+                    Geschiedenis van <span class="budget__title--month"><?php echo $month ?></span>:
+                </div>
+            </div>
+        </div>
+
+        
+        
+        
+        <div class="bottom">
+
+
+            <form action="#" method="post" enctype="multipart/form-data" class="middle">
+                <select class="add__type" name="select_month">
+                    <?php
+
+                        // Uit de database worden de beschikbare maanden gehaald
+                        $query = "SELECT DISTINCT budget_month FROM budget";
+                        $select_month = mysqli_query($connection, $query);
+
+                        while($row = mysqli_fetch_assoc($select_month)) {
+                        $month = $row['budget_month'];
+
+                        // Waardes in box zetten
+                        echo "<option value='$month'>$month</option>";
+
+                        }
+                    ?>
+                </select><br><br>
+
+
+               <br><br>
+            </form>
+
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Maand</th>
+                        <th>Wat?</th>
+                        <th>Beschrijving</th>
+                        <th>Bedrag</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+
+                <?php
+
+
+
+                if(isset($_POST['bekijk_maand'])) {
+
+                    // Bekijk de geselecteerde maand
+                    $month = $_POST['select_month'];
+
+                    // Query
+                    $query = "SELECT * FROM budget WHERE budget_month = '$month'";
+                    $select_posts = mysqli_query($connection,$query);
+
+
+                    // Tabel vullen met data
+                    while($row = mysqli_fetch_assoc($select_posts)) {
+                        $budget_id = $row['budget_id'];
+                        $budget_month = $row['budget_month'];
+                        $budget_beschrijving = $row['budget_beschrijving'];
+                        $budget_bedrag = $row['budget_bedrag'];
+                        $budget_wat = $row['budget_wat'];
+
+                        echo "<tr>";
+                        echo "<td>$budget_month</td>";
+                        echo "<td>$budget_wat</td>";
+                        echo "<td>$budget_beschrijving</td>";
+                        echo "<td>$budget_bedrag</td>";
+                        echo "</tr>";
+                    }
+
+                }
+
+
+                ?>
+
+
+
+                </tbody>
+            </table><br><br>
+
+
+          
+
+
+
+             <div class="top">
+            <div class="budget">
+                <div class="budget__title">
+                    Totaal gespaard in <span class="budget__title--month"><?php echo $month ?></span>:
+                </div>
+                
+                <div class="budget__income clearfix">
+                    <div class="budget__income--text">Inkomen</div>
+                    <div class="right">
+                        <div class="budget__income--value">
+                            <?php
+
+                            // Hieronder geef je aan wat er weergegeven moet worden
+                            // Dit is synchroom aan wat er in de database staat
+                            $what = "Inkomen";
+
+                            // Query
+                            $query_income = "SELECT * FROM budget WHERE budget_wat = '$what' AND budget_month = '$month'";
+                            $query = mysqli_query($connection, $query_income);
+
+                            // Alle waardes worden in een array gestopt
+                            while($row = mysqli_fetch_assoc($query)) {
+                                $value_inkom[] = $row['budget_bedrag'];;
+                            }
+
+                            // Alle waardes in de array worden bij elkaar opgeteld
+                            $value_inkomen = array_sum($value_inkom);
+
+                            echo $value_inkomen;
+
+                            ?>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="budget__expenses clearfix">
+                    <div class="budget__expenses--text">Uitgaven</div>
+                    <div class="right clearfix">
+                        <div class="budget__expenses--value">
+                            <?php
+
+                            // Hieronder geef je aan wat er weergegeven moet worden
+                            // Dit is synchroom aan wat er in de database staat
+                            $what = "Uitgaven";
+
+                            // Query
+                            $query_income = "SELECT * FROM budget WHERE budget_wat = '$what' AND budget_month = '$month'";
+                            $query = mysqli_query($connection, $query_income);
+
+                            // Alle waardes worden in een array gestopt
+                            while($row = mysqli_fetch_assoc($query)) {
+                                $value_uitgav[] = $row['budget_bedrag'];;
+                            }
+
+                            // Alle waardes in de array worden bij elkaar opgeteld
+                            $value_uitgaven = array_sum($value_uitgav);
+
+                            echo $value_uitgaven;
+
+                            ?>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="budget__value">
+                    <?php
+
+                    // Berekening van het beschikbare budget
+                    echo $value_inkomen - $value_uitgaven;
+
+                    ?>
+                </div>
+            </div>
+        </div>
+        
+
+            <div class="middle">
+                <a href="index.php"><button>Ga terug</button></a>
+            </div>
+
+
+        </div>
+        
+        <script src="budget.js"></script>
+    </body>
+</html>
